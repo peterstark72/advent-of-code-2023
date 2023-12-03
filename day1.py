09b42816bbaa49aed142dbc7d166e44ccae405d3
Peter Stark <peterstark72@gmail.com>
2023-12-03 09:45:15 +0100

    day 1, part 2

--- day1.py
+++ day1.py
@@ -8,9 +8,20 @@ def part_one(lines : list):
     return sum(combines)
 
 
+def part_two(lines : list):
+    literals = ["one", "two", "three", "four", "five", "six", "seven", "eight", "nine"]
+    regex_numbers = r'(?=(\d|one|two|three|four|five|six|seven|eight|nine))'
+
+    numbers = [list(map(lambda n: literals.index(n) + 1 if len(n) > 1 else int(n), re.findall(regex_numbers, line))) for line in lines]
+    combines = [int("".join([str(num[0]) + str(num[-1])])) for num in numbers]
+
+    return sum(combines)
+
+
 def main():
     input = open('day1_input.txt').readlines()
-    print(part_one(input))
+    print("Day 1 p1: ", part_one(input))
+    print("Day 1 p2: ", part_two(input))
 
 
 if __name__ == '__main__':
--- day1_test.py
+++ day1_test.py
@@ -2,16 +2,26 @@ import unittest
 import day1
 import os
 
-input = '''1abc2
-pqr3stu8vwx
-a1b2c3d4e5f
-treb7uchet'''.splitlines()
 
 class TestDay1(unittest.TestCase):
     
         def test_part1(self):
+            input = '''1abc2
+            pqr3stu8vwx
+            a1b2c3d4e5f
+            treb7uchet'''.splitlines()
             self.assertEqual(day1.part_one(input), 142)
     
+        def test_part2(self):
+            input = '''two1nine
+            eightwothree
+            abcone2threexyz
+            xtwone3four
+            4nineeightseven2
+            zoneight234
+            7pqrstsixteen'''.splitlines()
+            self.assertEqual(day1.part_two(input), 281)
+
 
 
 if __name__ == '__main__':
